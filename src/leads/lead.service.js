6f0a6b07471a70dbc7db45cc1d3e8281fc0aa5d5
Siddhant-0107 <cvm02874@gmail.com>
2026-09-07 19:12:17 +0530

    Return all normalized scraped leads

--- src/leads/lead.service.js
+++ src/leads/lead.service.js
@@ -2,7 +2,8 @@ import { Lead } from "./lead.model.js";
 import { deduplicate } from "./deduplicator.js";
 import { normalizeLead } from "./normalizer.js";
 export async function persistLeads(rawLeads, jobId) {
-  const leads = deduplicate(rawLeads.map(normalizeLead)); let stored = 0; let duplicates = rawLeads.length - leads.length;
+  const normalizedRaw = rawLeads.map(normalizeLead);
+  const leads = deduplicate(normalizedRaw); let stored = 0; let duplicates = rawLeads.length - leads.length;
   for (const lead of leads) { try { await Lead.create({ ...lead, jobId }); stored += 1; } catch (error) { if (error?.code === 11000) duplicates += 1; else throw error; } }
-  return { stored, duplicates, normalized: leads };
+  return { stored, duplicates, normalized: leads, normalizedRaw };
 }
