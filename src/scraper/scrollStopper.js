87a65990582166d11d5268dcd9d57b6b6e36b028
Siddhant-0107 <cvm02874@gmail.com>
2026-09-07 21:16:21 +0530

    Remove obsolete scroll stopper utility

--- src/scraper/scrollStopper.js
+++
@@ -1,6 +0,0 @@
-export function shouldContinueScrolling(iteration, currentListingCount, previousListingCount, maxResults, maxScrollIterations, stagnantIterations = 0) {
-  if (iteration >= maxScrollIterations) return false;
-  if (currentListingCount >= maxResults) return false;
-  if (iteration > 0 && stagnantIterations >= 3) return false;
-  return true;
-}
